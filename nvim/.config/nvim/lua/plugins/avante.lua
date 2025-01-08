5d5a0ebd05327181e476bb7e8f75bc38384a18e7
Carlos López <karlos1337@gmail.com>
2025-01-08 17:53:37 +0100

    refactor: replace code-companion with avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,48 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = "*", -- set this to "*" if you want to always pull the latest change, false to update on release
+  opts = {
+    provider = "claude",
+    auto_suggestions_provider = "claude",
+    claude = {
+      model = "claude-3-5-sonnet-20241022",
+      endpoint = "https://api.anthropic.com",
+      temperature = 0,
+      max_tokens = 4096,
+    },
+  },
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+    { "AstroNvim/astrocore", opts = function(_, opts) opts.mappings.n["<Leader>a"] = { desc = " Avante" } end },
+  },
+}
